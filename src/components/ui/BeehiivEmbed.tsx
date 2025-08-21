329be89bb8f6af1f8a35cb5debad737bbaa9c4e3
Ben Stauffer <bhstauffer@gmail.com>
2025-08-20 19:46:48 -0700

    beehiiv

--- src/components/ui/BeehiivEmbed.tsx
+++ src/components/ui/BeehiivEmbed.tsx
@@ -1,34 +1,25 @@
 'use client'
 
-import React, { useState, useEffect } from 'react'
 import Script from 'next/script'
 
 export function BeehiivEmbed() {
-    const [isMounted, setIsMounted] = useState(false)
-
-    useEffect(() => {
-        setIsMounted(true)
-    }, [])
-
-    if (!isMounted) {
-        return null
-    }
-
     return (
         <div className="w-full flex justify-center mt-8 mb-8">
-            <Script src="https://subscribe-forms.beehiiv.com/embed.js" strategy="afterInteractive" />
-            <iframe
-                src="https://subscribe-forms.beehiiv.com/b838089a-71e5-47b9-bab7-cdfd44a8de85"
-                className="beehiiv-embed"
-                data-test-id="beehiiv-embed"
-                frameBorder={0}
+            <Script async src="https://subscribe-forms.beehiiv.com/embed.js" />
+            <iframe 
+                src="https://subscribe-forms.beehiiv.com/b838089a-71e5-47b9-bab7-cdfd44a8de85" 
+                className="beehiiv-embed" 
+                data-test-id="beehiiv-embed" 
+                frameBorder="0" 
+                scrolling="no" 
                 style={{ 
-                    width: '400px',
-                    height: '309px',
-                    maxWidth: '100%',
-                    borderRadius: 0, 
+                    width: '100%', 
+                    height: '158px', 
+                    margin: 0, 
+                    borderRadius: '0px 0px 0px 0px', 
                     backgroundColor: 'transparent', 
-                    boxShadow: '0 0 #0000'
+                    boxShadow: '0 0 #0000', 
+                    maxWidth: '100%' 
                 }}
             />
         </div>
