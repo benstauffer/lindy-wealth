1d4655362d0656a876ec96c9a3c3eab69659fe0e
Ben Stauffer <bhstauffer@gmail.com>
2025-09-05 21:14:34 -0700

    yup

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -52,8 +52,8 @@ export default function FAQ() {
                 </p>
                 <p className="text-black/80 text-base ml-4 mt-2">
                   Pricing:<br />
-                  One-time Plan: starting at $3,000.<br />
-                  Ongoing Planning: $5,000-$10,000/year.
+                  One-time Plan: starting at $8,000.<br />
+                  Ongoing Planning: $10,000/year.
                 </p>
               </div>
 
