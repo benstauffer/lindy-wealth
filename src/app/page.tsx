431ddd29dafb7cebbecd407b74c06876ebc914bd
Ben Stauffer <bhstauffer@gmail.com>
2025-09-13 00:53:11 -0700

    q

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,13 @@ import { RiArrowRightLine } from "react-icons/ri";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col overflow-x-hidden">
       <HeaderSection />
       
-      <div className="flex-1 flex items-center justify-center pt-20 md:pt-0">
-        <div className="flex flex-col items-center justify-center max-w-lg mx-auto px-6">
+      <div className="flex-1 flex items-center justify-center pt-32 md:pt-0">
+        <div className="flex flex-col items-center justify-center w-full max-w-lg mx-auto px-6">
           {/* Text section above */}
-          <div className="text-white text-center">
+          <div className="text-white text-center mb-6">
             <h1 className="text-3xl md:text-5xl mb-4">
               Secure Your 
               <br/>
@@ -29,8 +29,10 @@ export default function Home() {
           </div>
           
           {/* Diagram section */}
-          <div className="flex justify-center">
-            <AnimatedVennDiagram />
+          <div className="flex justify-center w-full overflow-hidden">
+            <div className="scale-50 md:scale-100">
+              <AnimatedVennDiagram />
+            </div>
           </div>
         </div>
       </div>
